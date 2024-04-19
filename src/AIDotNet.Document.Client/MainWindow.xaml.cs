d9609b2eb1896ba011d7a1b2e5f1c8422d84170c
token <239573049@qq.com>
2024-04-19 10:32:23 +0800

    新增Wpf客户端

---
+++ src/AIDotNet.Document.Client/MainWindow.xaml.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System.Windows;
+
+namespace AIDotNet.Document.Client
+{
+	/// <summary>
+	/// Interaction logic for MainWindow.xaml
+	/// </summary>
+	public partial class MainWindow : Window
+	{
+		public MainWindow()
+		{
+			InitializeComponent();
+
+			var services = new ServiceCollection();
+			services.AddWpfBlazorWebView();
+			services.AddDocumentRcl();
+			services.AddDocumentService();
+#if DEBUG
+			services.AddBlazorWebViewDeveloperTools();
+#endif
+
+			BlazroWeb.RootComponents.Add(new Microsoft.AspNetCore.Components.WebView.Wpf.RootComponent()
+			{
+				Selector = "#app",
+				ComponentType = typeof(Document.App),
+			});
+
+			var app = services.BuildServiceProvider();
+
+
+			Resources.Add("services", app);
+		}
+	}
+}
